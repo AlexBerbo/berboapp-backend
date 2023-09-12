f167ffd3040be48d2967bc021dd930b2da254d9e
alexberbo <berbo997@gmail.com>
2023-09-12 21:24:23 +0200

    third

--- src/main/java/tech/alexberbo/berboapp/BerboappApplication.java
+++ src/main/java/tech/alexberbo/berboapp/BerboappApplication.java
@@ -2,7 +2,6 @@ package tech.alexberbo.berboapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -26,7 +25,7 @@ public class BerboappApplication {
 	public CorsFilter corsFilter() { // Cors filter, so cors does not interrupt with all of our methods
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://berboapp.axb"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200/**", "http://localhost:3000"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization",
 				"Origin, Accept", "Jwt-Token", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization",
